b2a0a3be1f2e65596cbacd7d4cc9484071cbb295
ryanballani <ryan.ballani@gmail.com>
2025-07-18 16:41:12 -0400

    Add App.js component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>🏈 Fantasy Draft Tracker</h1>
+      <p>This is your starter app. Time to track those wins!</p>
+    </div>
+  );
+}
+
+export default App;
